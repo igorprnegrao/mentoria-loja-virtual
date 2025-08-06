429c510c8986e3213c493de5ed1a3b46f2082df1
igorprnegrao <neurocientista@gmail.com>
2025-08-06 02:46:05 -0300

    create-entity - marca produto

---
+++ src/main/java/com/lojavirtualmentoria/lojavirtual/model/MarcaProduto.java
@@ -0,0 +1,37 @@
+package com.lojavirtualmentoria.lojavirtual.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity  //JPA annotation = criar entidade
+@Table(name="marca_produto") // criar o nome da tabela
+@SequenceGenerator(name="seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+public class MarcaProduto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id //id da entidade
+    //usar o sequencegenerator id ser gerado na ordem 1, 2, 3, 4, etc..
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+    private Long id;
+
+    private String nomeDescricao;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeDescricao() {
+        return nomeDescricao;
+    }
+
+    public void setNomeDescricao(String nomeDescricao) {
+        this.nomeDescricao = nomeDescricao;
+    }
+}
